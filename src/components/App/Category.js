3667ac3b498abe8aee56c68deb8ccac5e0ba5a09
Artem Kovshov <artem.kovshov@kronos.com>
2016-12-02 17:54:25 +0300

    Started implementation of categories render.

--- src/components/App/Category.js
+++ src/components/App/Category.js
@@ -6,7 +6,7 @@ class Category extends Component {
     render() {
         return (
             <li style={this.props.style} className="App-main__category-item">
-                <label><input type="button" className="arrow_down icon" /><span className="category-name">This is category</span></label>
+                <label><input type="button" className="arrow_down icon" /><span className="category-name">{this.props.params.name}</span></label>
                 <input type="button" className="icon edit-button" />
                 <div className="item__button-block">
                     <input type="button" className="item__button remove-button icon" />
--- src/components/App/CategoryList.js
+++ src/components/App/CategoryList.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import Category from './Category';
 
 class CategoryList extends Component {
+
+
     render() {
         const sub = {
             left: '2em'
         };
 
+        const categories = [
+            {name: "High priority" , id: 1, parentID: null, select: false},
+            {name: "Medium priority" , id: 2, parentID: null, select: false},
+            {name: "Low priority" , id: 2, parentID: null, select: false}
+        ];
+
+
         return (
             <ul className="App-main__category-list">
-                <Category/>
-                <Category/>
-                <Category/>
+                {categories.map((elem) => <Category params={elem}/>)}
                 <Category style={sub}/>
                 <Category style={sub}/>
                 <Category style={sub}/>
